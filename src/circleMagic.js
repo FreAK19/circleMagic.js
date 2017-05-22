c4c08fd19064591a2c0d7a6068bd83813d22f5dd
FreAK19 <andrii.kiri4wncko@yandex.ru>
2017-05-22 15:45:25 +0300

    create basic enviroment and default parameters

---
+++ src/circleMagic.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Andrew on 22.05.2017.
+ */
+;(function($) {
+    $.fn.circleMagic = function(options) {
+
+        let settings = $.extend({
+            elem: '.header',
+            color: 'rgba(255,255,255,.4)',
+            radius: 20,
+            densety: 0.3,
+            clearOffset: 0.2
+        }, options);
+    }
+})(jQuery);
